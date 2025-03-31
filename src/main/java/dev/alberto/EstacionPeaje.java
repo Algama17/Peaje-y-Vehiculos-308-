f90b9118bf16c018fe6b25b9cf540048bc98d498
Algama17 <albertogarciamallada@gmail.com>
2025-03-31 18:10:06 +0200

    code listar

--- src/main/java/dev/alberto/EstacionPeaje.java
+++ src/main/java/dev/alberto/EstacionPeaje.java
@@ -1,18 +1,36 @@
 package dev.alberto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EstacionPeaje {
+    private String nombre;
+    private String ciudad;
     private double totalRecaudado;
+    private List<Vehiculo> vehiculos;
 
     public EstacionPeaje(String nombre, String ciudad) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
         this.totalRecaudado = 0.0;
+        this.vehiculos = new ArrayList<>();
     }
 
     public void registrarVehiculo(Vehiculo vehiculo) {
         double peaje = vehiculo.calcularPeaje();
         this.totalRecaudado += peaje;
+        this.vehiculos.add(vehiculo);
     }
 
     public double calcularTotalRecaudado() {
         return totalRecaudado;
+    }
+
+    public List<String> listarVehiculos() {
+        List<String> placas = new ArrayList<>();
+        for (Vehiculo vehiculo : vehiculos) {
+            placas.add(vehiculo.getPlaca());
+        }
+        return placas;
     }
 }
