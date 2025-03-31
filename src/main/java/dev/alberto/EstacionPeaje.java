ef492f252faffc62d6b17741c6ff5d63e1368930
Algama17 <albertogarciamallada@gmail.com>
2025-03-31 17:48:59 +0200

    first code

--- src/main/java/dev/alberto/EstacionPeaje.java
+++ src/main/java/dev/alberto/EstacionPeaje.java
@@ -1,5 +1,18 @@
 package dev.alberto;
 
 public class EstacionPeaje {
+    private double totalRecaudado;
 
+    public EstacionPeaje(String nombre, String ciudad) {
+        this.totalRecaudado = 0.0;
+    }
+
+    public void registrarVehiculo(Vehiculo vehiculo) {
+        double peaje = vehiculo.calcularPeaje();
+        this.totalRecaudado += peaje;
+    }
+
+    public double calcularTotalRecaudado() {
+        return totalRecaudado;
+    }
 }
