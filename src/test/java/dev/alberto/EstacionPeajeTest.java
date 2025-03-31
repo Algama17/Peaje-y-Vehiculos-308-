c3f078a47f347ca1cd0c5180d4e74769b9ec6b90
Algama17 <albertogarciamallada@gmail.com>
2025-03-31 18:05:46 +0200

    test listar

--- src/test/java/dev/alberto/EstacionPeajeTest.java
+++ src/test/java/dev/alberto/EstacionPeajeTest.java
@@ -1,6 +1,7 @@
 package dev.alberto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -29,5 +30,21 @@ public class EstacionPeajeTest {
         Vehiculo camion = new Camion("Camionion", 3);
         estacionPeaje.registrarVehiculo(camion);
         assertEquals(150.0, estacionPeaje.calcularTotalRecaudado());
+    }
+
+        
+    @Test
+    
+    void testListarVehiculos() {    
+        EstacionPeaje estacionPeaje = new EstacionPeaje("Peaje", "Ciudad");
+        Vehiculo coche = new Coche("asdf1234");
+        Vehiculo moto = new Moto("motoPedrosa");
+        Vehiculo camion = new Camion("Camionion", 3);
+        estacionPeaje.registrarVehiculo(coche);
+        estacionPeaje.registrarVehiculo(moto);
+        estacionPeaje.registrarVehiculo(camion);
+        assertTrue(estacionPeaje.listarVehiculos().contains("asdf1234"));
+        assertTrue(estacionPeaje.listarVehiculos().contains("motoPedrosa"));
+        assertTrue(estacionPeaje.listarVehiculos().contains("Camionion"));
     }
 }
